5e32b69edb6da08c540fe07a847639c8b54331b2
Ivan <xcaliburr24@gmail.com>
2026-05-08 19:55:37 +0300

    logig task

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+const tabContainers = document.querySelectorAll('.tabs');
+tabContainers.forEach(container => {
+  const tabs = container.querySelectorAll('.tab');
+  const contents = container.querySelectorAll('.tab__content');
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(t => t.classList.remove('tab_active'));
+      tab.classList.add('tab_active');
+      contents.forEach(c => c.classList.remove('tab__content_active'));
+      if (contents[index]) {
+        contents[index].classList.add('tab__content_active');
+      }
+    });
+  });
+});
